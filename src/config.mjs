18cb3cfe720b6006de56499cfe9f6adf882e9f4b
na57 <na57@qq.com>
2018-04-25 14:12:21 +0800

    添加数据库连接字符串设置

--- src/config.mjs
+++ src/config.mjs
@@ -2,4 +2,4 @@ export const port = process.env.PORT || 3000;
 export const host = process.env.WEBSITE_HOSTNAME || `localhost:${port}`;
 export const secret = process.env.SECRET || 'secret';
 
-export const mongoUrl = 'mongodb://localhost/uir';
+export const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/uir';
